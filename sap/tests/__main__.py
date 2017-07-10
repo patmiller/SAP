43befa6410be3a0e4b3d14e01f86e719ee843fd1
patmiller <patrick.miller@gmail.com>
2017-07-09 21:51:15 -0500

    stubbed addfunction

---
+++ sap/tests/__main__.py
@@ -0,0 +1,80 @@
+"""
+>>> from sap.if1 import Module,IFBasic,IFArray,IFStream,IFMultiple,IFTuple,IFFunction
+>>> m = Module()
+>>> m
+<module>
+>>> m.types
+[boolean, character, doublereal, integer, null, real, wildbasic, wild]
+>>> map(int,m.types)
+[1, 2, 3, 4, 5, 6, 7, 8]
+>>> m.boolean
+boolean
+>>> m.integer
+integer
+>>> r = m.addtype(IFBasic,3) # doesn't make a new type!
+>>> r is m.integer
+True
+>>> m.pragmas['C'] = 'IF1 Check'
+>>> m.pragmas['D'] = 'Dataflow ordered'
+>>> m.pragmas['F'] = 'Python Frontend'
+>>> m.types
+[boolean, character, doublereal, integer, null, real, wildbasic, wild]
+>>> m.addtype(IFArray,m.integer)
+array[integer]
+>>> m.addtype(IFStream,m.real)
+stream[real]
+>>> m.addtype(IFMultiple,m.boolean)
+multiple[boolean]
+>>> ins = m.addtype(IFTuple,m.integer,m.integer)
+>>> outs = m.addtype(IFTuple,m.real)
+>>> m.addtype(IFFunction,ins,outs)
+(integer,integer)->(real)
+>>> m.addtype(IFFunction,(m.boolean,m.real),(m.real,m.real))
+(boolean,real)->(real,real)
+>>> m.addtype(IFFunction,m.integer,m.real)
+(integer)->(real)
+>>> m.addtype(IFFunction,None,m.real)
+->(real)
+>>> m.addtype(IFFunction,(),m.real)
+->(real)
+>>> for x in m.if1: print x
+T 1 1 0 %na=boolean
+T 2 1 1 %na=character
+T 3 1 2 %na=doublereal
+T 4 1 3 %na=integer
+T 5 1 4 %na=null
+T 6 1 5 %na=real
+T 7 1 6 %na=wildbasic
+T 8 10 %na=wild
+T 9 0 4
+T 10 6 6
+T 11 4 1
+T 12 8 4 0
+T 13 8 4 12
+T 14 8 6 0
+T 15 3 13 14
+T 16 8 1 14
+T 17 8 6 14
+T 18 3 16 17
+T 19 3 12 14
+T 20 3 0 14
+C$  C IF1 Check
+C$  D Dataflow ordered
+C$  F Python Frontend
+"""
+
+import sap.if1
+
+if __name__ == '__main__':
+    print sap.if1
+
+    import doctest
+    doctest.testmod()
+
+if 0:
+    print 'function',m.addtype(IFFunction,ins,outs)
+    print 'function',m.addtype(IFFunction,None,outs)
+    print 'function',m.addtype(IFFunction,(m.integer,m.integer),(m.boolean,))
+    print 'function',m.addtype(IFFunction,(),(m.boolean,))
+    print 'function',m.addtype(IFFunction,m.integer,m.boolean)
+    
