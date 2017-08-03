b51c8595e50229a710e6be7212d44cff53d96f4b
Girishvar Venkat <numbertheorist17@Girishvars-MacBook-Pro.local>
2017-08-03 11:30:25 -0500

    Updated IFX.h so that it would build

--- IFX.h
+++ IFX.h
@@ -13,13 +13,13 @@ extern PyObject* DEFAULT_OPCODES;
 
 #define STATIC_STR(name,value)  \
   static PyObject* name = nullptr;			\
-  if (!name) name = PyString_FromString(value);
+if (!name) name = PyString_FromString(value);
 
 #define TODO(msg) PyErr_Format(PyExc_NotImplementedError,"\n%s:%d: %s",__FILE__,__LINE__,msg)
- 
+
 class PyOwned {
   PyObject* P = nullptr;
- public:
+  public:
   explicit PyOwned(PyObject* P=nullptr) : P(P) {}
   PyOwned(PyOwned const&) = delete;
   PyOwned& operator=(PyOwned const&) = delete;
@@ -43,209 +43,210 @@ class PyOwned {
 
 template<class T>
 class IF1 : public std::enable_shared_from_this<IF1<T>> {
-public:
-  IF1() {}
-  virtual ~IF1() {}
+  public:
+    IF1() {}
+    virtual ~IF1() {}
 
-  struct python {
-    PyObject_HEAD
-    std::shared_ptr<T> cxx;
-  };
+    struct python {
+      PyObject_HEAD
+        std::shared_ptr<T> cxx;
+    };
 
-  static const long flags = Py_TPFLAGS_DEFAULT;
+    static const long flags = Py_TPFLAGS_DEFAULT;
 
-  std::shared_ptr<T> shared() {
-    return std::dynamic_pointer_cast<T>(this->shared_from_this());
-  }
-
-  static std::shared_ptr<T>& shared(PyObject* o) {
-    python* p = reinterpret_cast<python*>(o);
-    return p->self;
-  }
-
-  PyObject* package() {
-    auto p = std::dynamic_pointer_cast<T>(this->shared_from_this());
-
-    // Make a new Python object shell and use placement new to copy in
-    // the shared pointer
-    PyOwned  o(PyType_GenericNew(&T::Type,NULL,NULL));
-    if (!o) return nullptr;
-    new (&reinterpret_cast<python*>(o.borrow())->cxx) std::shared_ptr<T>(nullptr);
-    reinterpret_cast<python*>(o.borrow())->cxx = p;
-    return o.incref();
-  }
-
-  static int init(PyObject* pySelf,PyObject* args,PyObject* kwargs) {
-    // Set to a known state for the destructor
-    auto self = reinterpret_cast<python*>(pySelf);
-    new (&self->cxx) std::shared_ptr<T>(nullptr);
-
-    try {
-      self->cxx = std::make_shared<T>(self,args,kwargs);
+    std::shared_ptr<T> shared() {
+      return std::dynamic_pointer_cast<T>(this->shared_from_this());
     }
 
-    // Threw a C++ exception
-    catch (std::exception& e) {
-      PyErr_SetString(PyExc_RuntimeError,e.what());
-      return -1;
+    static std::shared_ptr<T>& shared(PyObject* o) {
+      python* p = reinterpret_cast<python*>(o);
+      return p->self;
     }
 
-    // Threw a Python exception
-    catch (PyObject*) {
-      return -1;
+    PyObject* package() {
+      auto p = std::dynamic_pointer_cast<T>(this->shared_from_this());
+
+      // Make a new Python object shell and use placement new to copy in
+      // the shared pointer
+      PyOwned  o(PyType_GenericNew(&T::Type,NULL,NULL));
+      if (!o) return nullptr;
+      new (&reinterpret_cast<python*>(o.borrow())->cxx) std::shared_ptr<T>(nullptr);
+      reinterpret_cast<python*>(o.borrow())->cxx = p;
+      return o.incref();
     }
 
-    return 0;
-  }
-   
-  static void dealloc(PyObject* pySelf) {
-    auto self = reinterpret_cast<python*>(pySelf);
-    auto p = &(self->cxx);
-    p->std::shared_ptr<T>::~shared_ptr<T>();
-    Py_TYPE(pySelf)->tp_free(pySelf);
-  }
+    static int init(PyObject* pySelf,PyObject* args,PyObject* kwargs) {
+      // Set to a known state for the destructor
+      auto self = reinterpret_cast<python*>(pySelf);
+      new (&self->cxx) std::shared_ptr<T>(nullptr);
 
-  static PyObject* getattro(PyObject* self,PyObject* attr) {
-    if (PyString_Check(attr) && PyString_GET_SIZE(attr) == 2) {
+      try {
+        self->cxx = std::make_shared<T>(self,args,kwargs);
+      }
+
+      // Threw a C++ exception
+      catch (std::exception& e) {
+        PyErr_SetString(PyExc_RuntimeError,e.what());
+        return -1;
+      }
+
+      // Threw a Python exception
+      catch (PyObject*) {
+        return -1;
+      }
+
+      return 0;
+    }
+
+    static void dealloc(PyObject* pySelf) {
+      auto self = reinterpret_cast<python*>(pySelf);
+      auto p = &(self->cxx);
+      using namespace std;
+      p->template shared_ptr<T>::~shared_ptr<T>();
+      Py_TYPE(pySelf)->tp_free(pySelf);
+    }
+
+    static PyObject* getattro(PyObject* self,PyObject* attr) {
+      if (PyString_Check(attr) && PyString_GET_SIZE(attr) == 2) {
+        auto cxx = reinterpret_cast<python*>(self)->cxx;
+        auto A = PyDict_GetItem(cxx->pragmas.borrow(),attr);
+        if (A) { Py_INCREF(A); return A; }
+      }
+      return PyObject_GenericGetAttr(self,attr);
+    }
+    static int setattro(PyObject* self,PyObject* attr,PyObject* rhs) {
+      if (PyString_Check(attr) && PyString_GET_SIZE(attr) == 2) {
+        // Use the local pragma dictionary to hold these
+        auto cxx = reinterpret_cast<python*>(self)->cxx;
+        auto __dict__ = cxx->pragmas.borrow();
+        if (rhs) {
+          PyDict_SetItem(__dict__,attr,rhs);
+          return 0;
+        } else if (PyDict_GetItem(__dict__,attr)) {
+          PyDict_DelItem(__dict__,attr);
+          return 0;
+        }
+      }
+      return PyObject_GenericSetAttr(self,attr,rhs);
+    }
+
+    static bool ready(PyObject* module,char const* name) {
+      PyOwned mname(PyObject_GetAttrString(module,"__name__"));
+      if (!mname) return false;
+      if (!PyString_Check(mname.borrow())) return false;
+      std::string module_name(PyString_AS_STRING(mname.borrow()));
+      std::string fully_qualified_name = module_name + '.' + name;
+
+      T::Type.tp_name = ::strcpy(new char[fully_qualified_name.size()+1],fully_qualified_name.c_str()); // Leaks
+      T::Type.tp_basicsize = sizeof(python);
+      T::Type.tp_doc = T::doc;
+      T::Type.tp_flags = T::flags;
+      T::Type.tp_base = T::basetype();
+      T::Type.tp_new = PyType_GenericNew;
+      T::Type.tp_dealloc = T::dealloc;
+      T::Type.tp_init = T::init;
+      T::Type.tp_str = T::Type.tp_repr = T::str;
+      if (T::methods[0].ml_name) {
+        // Only set up if we have methods defined
+        T::Type.tp_methods = T::methods;
+      }
+      if (T::getset[0].name) {
+        // Only set up if we have getset defined
+        T::Type.tp_getset = T::getset;
+      }
+      T::Type.tp_as_number = &T::as_number;
+      T::Type.tp_as_sequence = &T::as_sequence;
+
+      T::Type.tp_getattro = T::getattro;
+      T::Type.tp_setattro = T::setattro;
+
+      T::setup(); // Other goodies that the type wants
+
+      if (PyType_Ready(&T::Type) < 0) return false;
+      Py_INCREF(&T::Type);
+      PyModule_AddObject(module,name,reinterpret_cast<PyObject*>(&T::Type));
+      return true;
+    }
+
+    static void setup() {
+    }
+
+    static PyTypeObject* basetype() {
+      return nullptr;
+    }
+
+    static PyObject* str(PyObject* self) {
       auto cxx = reinterpret_cast<python*>(self)->cxx;
-      auto A = PyDict_GetItem(cxx->pragmas.borrow(),attr);
-      if (A) { Py_INCREF(A); return A; }
+      if (!cxx) return PyErr_Format(PyExc_RuntimeError,"broken ptr");
+      return cxx->string(self);
     }
-    return PyObject_GenericGetAttr(self,attr);
-  }
-  static int setattro(PyObject* self,PyObject* attr,PyObject* rhs) {
-    if (PyString_Check(attr) && PyString_GET_SIZE(attr) == 2) { 
-      // Use the local pragma dictionary to hold these
-      auto cxx = reinterpret_cast<python*>(self)->cxx;
-      auto __dict__ = cxx->pragmas.borrow();
-      if (rhs) {
-	PyDict_SetItem(__dict__,attr,rhs);
-	return 0;
-      } else if (PyDict_GetItem(__dict__,attr)) {
-	PyDict_DelItem(__dict__,attr);
-	return 0;
+    virtual PyObject* string(PyObject* self) {
+      char const* dot = ::rindex(self->ob_type->tp_name,'.');
+      return PyString_FromFormat("<%s>",dot?(dot+1):self->ob_type->tp_name);
+    }
+    static T* borrow(PyObject* o) {
+      auto p = reinterpret_cast<python*>(o);
+      return p->cxx.get();
+    }
+
+    static PyObject* pragma_string(PyObject* dict,PyObject* auxdict=nullptr) {
+      static PyObject* SPACEPERCENT = nullptr;
+      if (!SPACEPERCENT) SPACEPERCENT = PyString_FromString(" %");
+      static PyObject* EQUAL = nullptr;
+      if (!EQUAL) EQUAL = PyString_FromString("=");
+
+      // Get keys from the main dict.   We will sort these later
+      PyObject* sorted = PyList_New(0);
+      PyObject* key;
+      Py_ssize_t pos = 0;
+      while (PyDict_Next(dict, &pos, &key, nullptr)) {
+        // Only string keys of size 2 are pragmas
+        if (!PyString_Check(key) || PyString_GET_SIZE(key) != 2) continue;
+        if (PyList_Append(sorted,key) != 0) { Py_DECREF(sorted); return nullptr; }
       }
-    }
-    return PyObject_GenericSetAttr(self,attr,rhs);
-  }
 
-  static bool ready(PyObject* module,char const* name) {
-    PyOwned mname(PyObject_GetAttrString(module,"__name__"));
-    if (!mname) return false;
-    if (!PyString_Check(mname.borrow())) return false;
-    std::string module_name(PyString_AS_STRING(mname.borrow()));
-    std::string fully_qualified_name = module_name + '.' + name;
-
-    T::Type.tp_name = ::strcpy(new char[fully_qualified_name.size()+1],fully_qualified_name.c_str()); // Leaks
-    T::Type.tp_basicsize = sizeof(python);
-    T::Type.tp_doc = T::doc;
-    T::Type.tp_flags = T::flags;
-    T::Type.tp_base = T::basetype();
-    T::Type.tp_new = PyType_GenericNew;
-    T::Type.tp_dealloc = T::dealloc;
-    T::Type.tp_init = T::init;
-    T::Type.tp_str = T::Type.tp_repr = T::str;
-    if (T::methods[0].ml_name) {
-      // Only set up if we have methods defined
-      T::Type.tp_methods = T::methods;
-    }
-    if (T::getset[0].name) {
-      // Only set up if we have getset defined
-      T::Type.tp_getset = T::getset;
-    }
-    T::Type.tp_as_number = &T::as_number;
-    T::Type.tp_as_sequence = &T::as_sequence;
-
-    T::Type.tp_getattro = T::getattro;
-    T::Type.tp_setattro = T::setattro;
-
-    T::setup(); // Other goodies that the type wants
-
-    if (PyType_Ready(&T::Type) < 0) return false;
-    Py_INCREF(&T::Type);
-    PyModule_AddObject(module,name,reinterpret_cast<PyObject*>(&T::Type));
-    return true;
-  }
-
-  static void setup() {
-  }
-
-  static PyTypeObject* basetype() {
-    return nullptr;
-  }
-
-  static PyObject* str(PyObject* self) {
-    auto cxx = reinterpret_cast<python*>(self)->cxx;
-    if (!cxx) return PyErr_Format(PyExc_RuntimeError,"broken ptr");
-    return cxx->string(self);
-  }
-  virtual PyObject* string(PyObject* self) {
-    char const* dot = ::rindex(self->ob_type->tp_name,'.');
-    return PyString_FromFormat("<%s>",dot?(dot+1):self->ob_type->tp_name);
-  }
-  static T* borrow(PyObject* o) {
-    auto p = reinterpret_cast<python*>(o);
-    return p->cxx.get();
-  }
-
-  static PyObject* pragma_string(PyObject* dict,PyObject* auxdict=nullptr) {
-    static PyObject* SPACEPERCENT = nullptr;
-    if (!SPACEPERCENT) SPACEPERCENT = PyString_FromString(" %");
-    static PyObject* EQUAL = nullptr;
-    if (!EQUAL) EQUAL = PyString_FromString("=");
-
-    // Get keys from the main dict.   We will sort these later
-    PyObject* sorted = PyList_New(0);
-    PyObject* key;
-    Py_ssize_t pos = 0;
-    while (PyDict_Next(dict, &pos, &key, nullptr)) {
-      // Only string keys of size 2 are pragmas
-      if (!PyString_Check(key) || PyString_GET_SIZE(key) != 2) continue;
-      if (PyList_Append(sorted,key) != 0) { Py_DECREF(sorted); return nullptr; }
-    }
-
-    if (auxdict) {
-      pos = 0;
-      while (PyDict_Next(auxdict, &pos, &key, nullptr)) {
-	// Only string keys of size 2 are pragmas
-	if (!PyString_Check(key) || PyString_GET_SIZE(key) != 2) continue;
-	bool match = false;
-	for(ssize_t i=0; i<PyList_GET_SIZE(sorted); ++i) {
-	  int cmp = -1;
-	  if (PyObject_Cmp(key,PyList_GET_ITEM(sorted,i),&cmp) == 0 && cmp == 0) {
-	    match = true;
-	    break;
-	  }
-	}
-	if (!match) {
-	  if (PyList_Append(sorted,key) != 0) { Py_DECREF(sorted); return nullptr; }
-	}
+      if (auxdict) {
+        pos = 0;
+        while (PyDict_Next(auxdict, &pos, &key, nullptr)) {
+          // Only string keys of size 2 are pragmas
+          if (!PyString_Check(key) || PyString_GET_SIZE(key) != 2) continue;
+          bool match = false;
+          for(ssize_t i=0; i<PyList_GET_SIZE(sorted); ++i) {
+            int cmp = -1;
+            if (PyObject_Cmp(key,PyList_GET_ITEM(sorted,i),&cmp) == 0 && cmp == 0) {
+              match = true;
+              break;
+            }
+          }
+          if (!match) {
+            if (PyList_Append(sorted,key) != 0) { Py_DECREF(sorted); return nullptr; }
+          }
+        }
       }
-    }
-    if (PyList_Sort(sorted) != 0) { Py_DECREF(sorted); return nullptr; }
+      if (PyList_Sort(sorted) != 0) { Py_DECREF(sorted); return nullptr; }
 
-    PyOwned result(PyString_FromString(""));
-    for(ssize_t i=0; i < PyList_GET_SIZE(sorted); ++i) {
-      PyObject* key /*borrowed*/ = PyList_GET_ITEM(sorted,i);
-      PyObject* pragma /*borrowed*/ = PyDict_GetItem(dict,key);
-      if (!pragma && auxdict) {
-	pragma = PyDict_GetItem(auxdict,key);
+      PyOwned result(PyString_FromString(""));
+      for(ssize_t i=0; i < PyList_GET_SIZE(sorted); ++i) {
+        PyObject* key /*borrowed*/ = PyList_GET_ITEM(sorted,i);
+        PyObject* pragma /*borrowed*/ = PyDict_GetItem(dict,key);
+        if (!pragma && auxdict) {
+          pragma = PyDict_GetItem(auxdict,key);
+        }
+        if (!pragma) continue;
+
+        PyString_Concat(result.addr(),SPACEPERCENT);
+        if (!result) return nullptr;
+        PyString_Concat(result.addr(),key);
+        if (!result) return nullptr;
+        PyString_Concat(result.addr(),EQUAL);
+        if (!result) return nullptr;
+        PyObject* rhs /*borrowed*/ = PyObject_Str(pragma);
+        if (!rhs) return nullptr;
+        PyString_ConcatAndDel(result.addr(),rhs);
+        if (!result) return nullptr;
       }
-      if (!pragma) continue;
-
-      PyString_Concat(result.addr(),SPACEPERCENT);
-      if (!result) return nullptr;
-      PyString_Concat(result.addr(),key);
-      if (!result) return nullptr;
-      PyString_Concat(result.addr(),EQUAL);
-      if (!result) return nullptr;
-      PyObject* rhs /*borrowed*/ = PyObject_Str(pragma);
-      if (!rhs) return nullptr;
-      PyString_ConcatAndDel(result.addr(),rhs);
-      if (!result) return nullptr;
+      return result.incref();
     }
-    return result.incref();
-  }
 };
 
 enum IFTypeCode {
